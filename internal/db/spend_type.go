ee7329e936fde113a9e92b36f5eced9d04a225e2
Shoshin Nikita <shoshin.nikita@gmail.com>
2019-10-27 22:56:10 +0300

    internal/db: add methods to work with SpendType

--- internal/db/spend_type.go
+++ internal/db/spend_type.go
@@ -7,7 +7,10 @@ import (
 	"github.com/pkg/errors"
 )
 
-var _ orm.BeforeInsertHook = (*SpendType)(nil)
+var (
+	_ orm.BeforeInsertHook = (*SpendType)(nil)
+	_ orm.BeforeUpdateHook = (*SpendType)(nil)
+)
 
 // SpendType contains information about spend type
 type SpendType struct {
@@ -24,10 +27,47 @@ func (in *SpendType) BeforeInsert(ctx context.Context) (context.Context, error) 
 	return ctx, nil
 }
 
+func (in *SpendType) BeforeUpdate(ctx context.Context) (context.Context, error) {
+	return in.BeforeInsert(ctx)
+}
+
 // -----------------------------------------------------------------------------
 
-func (db DB) AddSpendType() {}
+// AddSpendType adds new Spend Type
+func (db DB) AddSpendType(name string) (typeID uint, err error) {
+	spendType := &SpendType{Name: name}
+	err = db.db.Insert(spendType)
+	if err != nil {
+		err = errors.Wrap(err, "can't insert a new Spend Type")
+		db.log.Error(err)
+		return 0, err
+	}
 
-func (db DB) EditSpendType() {}
+	return spendType.ID, nil
+}
 
-func (db DB) RemoveSpendType() {}
+// EditSpendType modifies existing Spend Type
+func (db DB) EditSpendType(id uint, newName string) error {
+	spendType := &SpendType{ID: id, Name: newName}
+	err := db.db.Update(spendType)
+	if err != nil {
+		err = errors.Wrap(err, "can't insert a new Spend Type")
+		db.log.Error(err)
+		return err
+	}
+
+	return nil
+}
+
+// RemoveSpendType removes Spend Type with passed id
+func (db DB) RemoveSpendType(id uint) error {
+	spendType := &SpendType{ID: id}
+	err := db.db.Delete(spendType)
+	if err != nil {
+		err = errors.Wrapf(err, "can't delete spend type with id '%d'", id)
+		db.log.Error(err)
+		return err
+	}
+
+	return nil
+}
---
+++ internal/db/spend_type_test.go
@@ -0,0 +1,152 @@
+package db
+
+import (
+	"testing"
+
+	"github.com/go-pg/pg/v9"
+	"github.com/stretchr/testify/require"
+)
+
+func TestAddSpendType(t *testing.T) {
+	require := require.New(t)
+
+	// Init db
+	db := initDB(require)
+	defer db.Shutdown()
+
+	spendTypes := []struct {
+		SpendType
+		isError bool
+	}{
+		{
+			SpendType: SpendType{
+				ID:   1,
+				Name: "first type",
+			},
+		},
+		{
+			SpendType: SpendType{
+				ID:   2,
+				Name: "второй тип",
+			},
+		},
+		{
+			SpendType: SpendType{
+				ID:   0,
+				Name: "",
+			},
+			isError: true,
+		},
+	}
+
+	// Add Spend Types
+	for _, t := range spendTypes {
+		id, err := db.AddSpendType(t.Name)
+		if t.isError {
+			require.NotNil(err)
+			continue
+		}
+		require.Nil(err)
+		require.Equal(t.ID, id)
+	}
+
+	// Check Spend Types
+	for _, t := range spendTypes {
+		spendType := &SpendType{ID: t.ID}
+		err := db.db.Select(spendType)
+		if t.isError {
+			require.Equal(pg.ErrNoRows, err)
+			continue
+		}
+		require.Nil(err)
+	}
+}
+
+func TestEditSpendType(t *testing.T) {
+	require := require.New(t)
+
+	// Init db
+	db := initDB(require)
+	defer db.Shutdown()
+
+	spendTypes := []struct {
+		origin  SpendType
+		edited  SpendType
+		isError bool
+	}{
+		{
+			origin: SpendType{ID: 1, Name: "first type"},
+			edited: SpendType{ID: 1, Name: "new name"},
+		},
+		{
+			origin:  SpendType{ID: 2, Name: "first type"},
+			edited:  SpendType{ID: 2, Name: ""},
+			isError: true,
+		},
+	}
+
+	// Add Spend Types
+	for _, t := range spendTypes {
+		id, err := db.AddSpendType(t.origin.Name)
+		require.Nil(err)
+		require.Equal(t.origin.ID, id)
+	}
+
+	// Edit Spend Types
+	for _, t := range spendTypes {
+		err := db.EditSpendType(t.edited.ID, t.edited.Name)
+		if t.isError {
+			require.NotNil(err)
+			continue
+		}
+		require.Nil(err)
+	}
+
+	// Check Spend Types
+	for _, t := range spendTypes {
+		spendType := &SpendType{ID: t.origin.ID}
+		err := db.db.Select(spendType)
+		if t.isError {
+			require.Equal(t.origin, *spendType)
+			continue
+		}
+		require.Nil(err)
+		require.Equal(t.edited, *spendType)
+	}
+}
+
+func TestDeleteSpendType(t *testing.T) {
+	require := require.New(t)
+
+	// Init db
+	db := initDB(require)
+	defer db.Shutdown()
+
+	spendTypes := []SpendType{
+		{ID: 1, Name: "first type"},
+		{ID: 2, Name: "второй тип"},
+	}
+
+	// Add Spend Types
+	for _, t := range spendTypes {
+		id, err := db.AddSpendType(t.Name)
+		require.Nil(err)
+		require.Equal(t.ID, id)
+	}
+
+	// Delete all Spend Type
+	for _, t := range spendTypes {
+		db.RemoveSpendType(t.ID)
+	}
+
+	// Check Spend Types
+	for _, t := range spendTypes {
+		spendType := &SpendType{ID: t.ID}
+		err := db.db.Select(spendType)
+		require.Equal(pg.ErrNoRows, err)
+	}
+
+	// Try to delete Spend Type with invalid id
+	err := db.RemoveSpendType(20)
+	require.NotNil(err)
+}
