d3beaa007eab94269c871f715d15cbfad2d8a48b
Shoshin Nikita <shoshin.nikita@gmail.com>
2019-11-16 01:10:33 +0300

    internal/web: add tests for Monthly Payment handlers

--- internal/web/handlers_test.go
+++ internal/web/handlers_test.go
@@ -146,6 +146,8 @@ func TestHandlers_Income(t *testing.T) {
 					response.Body.Close()
 
 					require.Equal(wantResp, *resp)
+				default:
+					require.Fail("invalid resp type")
 				}
 			})
 		}
@@ -300,6 +302,301 @@ func TestHandlers_Income(t *testing.T) {
 
 				// Send Request
 				server.RemoveIncome(w, request)
+
+				// Check Response
+				response := w.Result()
+				require.Equal(tt.statusCode, response.StatusCode)
+
+				resp := &models.Response{}
+				decodeResponse(require, response.Body, resp)
+				response.Body.Close()
+
+				require.Equal(tt.resp, *resp)
+			})
+		}
+	})
+}
+
+func TestHandlers_MonthlyPayment(t *testing.T) {
+	requireGlobal := require.New(t)
+	server := initServer(requireGlobal)
+	defer stopServer(requireGlobal, server)
+
+	t.Run("AddMonthlyPayment", func(t *testing.T) {
+		tests := []struct {
+			desc       string
+			req        models.AddMonthlyPaymentReq
+			statusCode int
+			resp       interface{}
+		}{
+			{
+				desc: "valid request",
+				req: models.AddMonthlyPaymentReq{
+					MonthID: 1,
+					Title:   "Patreon",
+					Cost:    750,
+				},
+				statusCode: http.StatusOK,
+				resp: models.AddMonthlyPaymentResp{
+					Response: models.Response{
+						Success: true,
+					},
+					ID: 1,
+				},
+			},
+			{
+				desc: "valid request with notes and type",
+				req: models.AddMonthlyPaymentReq{
+					MonthID: 1,
+					Title:   "Rent",
+					Notes:   "some notes",
+					TypeID:  1,
+					Cost:    7000,
+				},
+				statusCode: http.StatusOK,
+				resp: models.AddMonthlyPaymentResp{
+					Response: models.Response{
+						Success: true,
+					},
+					ID: 2,
+				},
+			},
+			// Invalid requests
+			{
+				desc: "invalid request (empty title)",
+				req: models.AddMonthlyPaymentReq{
+					MonthID: 1,
+					Title:   "",
+					Notes:   "some notes",
+					Cost:    15000,
+				},
+				statusCode: http.StatusBadRequest,
+				resp: models.Response{
+					Error:   "bad params",
+					Success: false,
+				},
+			},
+			{
+				desc: "invalid request (zero cost)",
+				req: models.AddMonthlyPaymentReq{
+					MonthID: 1,
+					Title:   "title",
+					Notes:   "some notes",
+					Cost:    0,
+				},
+				statusCode: http.StatusBadRequest,
+				resp: models.Response{
+					Error:   "bad params",
+					Success: false,
+				},
+			},
+			{
+				desc: "invalid request (invalid month)",
+				req: models.AddMonthlyPaymentReq{
+					MonthID: 10,
+					Title:   "title",
+					Notes:   "some notes",
+					Cost:    15000,
+				},
+				statusCode: http.StatusBadRequest,
+				resp: models.Response{
+					Error:   "bad params",
+					Success: false,
+				},
+			},
+		}
+
+		for _, tt := range tests {
+			tt := tt
+
+			t.Run(tt.desc, func(t *testing.T) {
+				// Prepare
+				require := require.New(t)
+				w := httptest.NewRecorder()
+
+				// Prepare request
+				body := encodeRequest(require, tt.req)
+				request := httptest.NewRequest("POST", "/api/monthly-payments", body)
+
+				// Send Request
+				server.AddMonthlyPayment(w, request)
+
+				// Check Response
+				response := w.Result()
+				require.Equal(tt.statusCode, response.StatusCode)
+
+				wantResp := tt.resp
+				switch wantResp.(type) {
+				case models.AddMonthlyPaymentResp:
+					resp := &models.AddMonthlyPaymentResp{}
+					decodeResponse(require, response.Body, resp)
+					response.Body.Close()
+
+					require.Equal(wantResp, *resp)
+				case models.Response:
+					resp := &models.Response{}
+					decodeResponse(require, response.Body, resp)
+					response.Body.Close()
+
+					require.Equal(wantResp, *resp)
+				default:
+					require.Fail("invalid resp type")
+				}
+			})
+		}
+	})
+
+	t.Run("EditMonthlyPayment", func(t *testing.T) {
+		newTitle := func(s string) *string { return &s }
+		newNotes := func(s string) *string { return &s }
+		newTypeID := func(u uint) *uint { return &u }
+		newCost := func(i int64) *int64 { return &i }
+
+		tests := []struct {
+			desc       string
+			req        models.EditMonthlyPaymentReq
+			statusCode int
+			resp       models.Response
+		}{
+			{
+				desc: "edit title",
+				req: models.EditMonthlyPaymentReq{
+					ID:    1,
+					Title: newTitle("edited title"),
+				},
+				statusCode: http.StatusOK,
+				resp: models.Response{
+					Success: true,
+				},
+			},
+			{
+				desc: "edit all fields",
+				req: models.EditMonthlyPaymentReq{
+					ID:     2,
+					Title:  newTitle("edited title"),
+					Notes:  newNotes("updated notes"),
+					TypeID: newTypeID(1),
+					Cost:   newCost(123456),
+				},
+				statusCode: http.StatusOK,
+				resp: models.Response{
+					Success: true,
+				},
+			},
+			// Invalid requests
+			{
+				desc: "invalid request (invalid id)",
+				req: models.EditMonthlyPaymentReq{
+					ID:    5,
+					Title: newTitle("edited title"),
+					Notes: newNotes("updated notes"),
+					Cost:  newCost(10),
+				},
+				statusCode: http.StatusBadRequest,
+				resp: models.Response{
+					Error:   "bad params",
+					Success: false,
+				},
+			},
+			{
+				desc: "invalid request (zero cost)",
+				req: models.EditMonthlyPaymentReq{
+					ID:    2,
+					Title: newTitle("edited title"),
+					Notes: newNotes("updated notes"),
+					Cost:  newCost(0),
+				},
+				statusCode: http.StatusBadRequest,
+				resp: models.Response{
+					Error:   "bad params",
+					Success: false,
+				},
+			},
+		}
+
+		for _, tt := range tests {
+			tt := tt
+
+			t.Run(tt.desc, func(t *testing.T) {
+				// Prepare
+				require := require.New(t)
+				w := httptest.NewRecorder()
+
+				// Prepare request
+				body := encodeRequest(require, tt.req)
+				request := httptest.NewRequest("PUT", "/api/monthly-payments", body)
+
+				// Send Request
+				server.EditMonthlyPayment(w, request)
+
+				// Check Response
+				response := w.Result()
+				require.Equal(tt.statusCode, response.StatusCode)
+
+				resp := &models.Response{}
+				decodeResponse(require, response.Body, resp)
+				response.Body.Close()
+
+				require.Equal(tt.resp, *resp)
+			})
+		}
+	})
+
+	t.Run("RemoveMonthlyPayment", func(t *testing.T) {
+		tests := []struct {
+			desc       string
+			req        models.RemoveMonthlyPaymentReq
+			statusCode int
+			resp       models.Response
+		}{
+			{
+				desc: "remove Monthly Payment with id '1'",
+				req: models.RemoveMonthlyPaymentReq{
+					ID: 1,
+				},
+				statusCode: http.StatusOK,
+				resp: models.Response{
+					Success: true,
+				},
+			},
+			{
+				desc: "remove Monthly Payment with id '2'",
+				req: models.RemoveMonthlyPaymentReq{
+					ID: 2,
+				},
+				statusCode: http.StatusOK,
+				resp: models.Response{
+					Success: true,
+				},
+			},
+			// Invalid requests
+			{
+				desc: "remove Monthly Payment with id '3'",
+				req: models.RemoveMonthlyPaymentReq{
+					ID: 3,
+				},
+				statusCode: http.StatusBadRequest,
+				resp: models.Response{
+					Error:   "bad params",
+					Success: false,
+				},
+			},
+		}
+
+		for _, tt := range tests {
+			tt := tt
+
+			t.Run(tt.desc, func(t *testing.T) {
+				// Prepare
+				require := require.New(t)
+				w := httptest.NewRecorder()
+
+				// Prepare request
+				body := encodeRequest(require, tt.req)
+				request := httptest.NewRequest("DELETE", "/api/monthly-payment", body)
+
+				// Send Request
+				server.RemoveMonthlyPayment(w, request)
 
 				// Check Response
 				response := w.Result()
