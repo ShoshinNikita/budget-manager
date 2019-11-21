0f8a34726ff6889ee7bc3c1c70bb90faa8cf62e8
Shoshin Nikita <shoshin.nikita@gmail.com>
2019-11-21 20:57:51 +0300

    fix 'SERVER_PORT' env vars

--- scripts/local/run.sh
+++ scripts/local/run.sh
@@ -5,6 +5,6 @@ export DB_HOST="localhost"
 export DB_PORT="5432"
 export DB_USER="postgres"
 export DB_DATABASE="postgres"
-export SERVER_PORT=":8080"
+export SERVER_PORT="8080"
 
 go run -mod vendor cmd/budget_manager/main.go
