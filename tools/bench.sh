04785111ed02e2e74728a8884e701e2b9666dbc2
Shoshin Nikita <shoshin_nikita@fastmail.com>
2020-02-17 17:50:57 +0300

    tools/bench.sh: benchmark API instead of page
    Template execution consumes a lot of resources.
    So, the results are not very reliable

--- tools/bench.sh
+++ tools/bench.sh
@@ -3,10 +3,12 @@
 # via 'vegeta' for 5 seconds with 25 workers
 
 YEAR=$(date +"%Y")
-MONTH=$(date +"%m")
-URL="http://localhost:8080/overview/${YEAR}/${MONTH}"
+MONTH=$(date +"%-m")
+BODY='{"year": '${YEAR}', "month": '${MONTH}'}'
+URL="http://localhost:8080/api/months"
 DURATION="5s"
 #
+REQ_BODY_FILE="bench_body"
 RESULT_FILE="results.bin"
 
 # Check whether 'vegeta' is installed
@@ -23,13 +25,17 @@ if [ "$?" != "0" ]; then
 	exit 1
 fi
 
+# Prepare a file with test body
+echo ${BODY} > ${REQ_BODY_FILE}
+
 # Start benchmark
 echo "Start benchmark for '${URL}'..."
 
-echo "GET ${URL}" | vegeta attack -duration=${DURATION} -rate=0 -max-workers=25 > ${RESULT_FILE}
+echo "GET ${URL}" | vegeta attack -body=${REQ_BODY_FILE} -duration=${DURATION} -rate=0 -max-workers=25 > ${RESULT_FILE}
 
 cat ${RESULT_FILE} | vegeta report
 cat ${RESULT_FILE} | vegeta report -type="hist[25ms,50ms,100ms,200ms,400ms]"
 
 # Cleanup
 rm ${RESULT_FILE}
+rm ${REQ_BODY_FILE}
