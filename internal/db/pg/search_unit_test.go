b735853f0f84c0c22c3914382e7bc53b25253098
Shoshin Nikita <shoshin_nikita@fastmail.com>
2020-02-18 19:14:47 +0300

    internal/db/pg: set column names by struct tags

--- internal/db/pg/search_unit_test.go
+++ internal/db/pg/search_unit_test.go
@@ -25,7 +25,7 @@ func TestBuildSearchSpendsQuery(t *testing.T) {
 			       spend.title AS title, spend.notes AS notes, spend.cost AS cost,
 			       spend_type.id AS type__id, spend_type.name AS type__name
 
-			 FROM "spends" AS "spend"
+			 FROM spends AS "spend"
 			      INNER JOIN days AS day
 			      ON day.id = spend.day_id
 
