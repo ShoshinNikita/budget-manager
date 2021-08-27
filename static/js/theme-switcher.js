1ca679faf8c73f743ad3f12549b1600f19594eba
Shoshin Nikita <shoshin_nikita@fastmail.com>
2021-08-27 20:57:17 +0300

    support system theme
    * show 3 buttons in the footer: `Light Theme`, `System Theme` and `Dark Theme`
    * use the system theme by default
    * apply system theme change without a page reload

--- static/js/theme-switcher.js
+++ static/js/theme-switcher.js
@@ -1,36 +1,73 @@
 const localStorageKey = "theme";
 const themeChangeEventName = "theme-change";
+const darkThemeMedia = "(prefers-color-scheme: dark)";
 
 const lightTheme = "light";
+const systemTheme = "system";
 const darkTheme = "dark";
 
-function toggleTheme() {
+/**
+ * Saves a new theme in the local storage and updates the styles
+ */
+function switchTheme(newTheme) {
+	if (newTheme !== lightTheme && newTheme !== systemTheme && newTheme !== darkTheme) {
+		return;
+	}
+	localStorage.setItem(localStorageKey, newTheme);
+
+	updateStyles();
+}
+
+function updateStyles() {
 	let newTheme = lightTheme;
-	if (localStorage.getItem(localStorageKey) === lightTheme) {
+	if (isDarkTheme()) {
 		newTheme = darkTheme;
 	}
 
-	switchTheme(newTheme);
-}
+	document.getElementsByTagName("html")[0].setAttribute("data-theme", newTheme);
 
-function switchTheme(newTheme) {
-	const themeAttrName = "data-theme";
-
-	localStorage.setItem(localStorageKey, newTheme);
-	document.getElementsByTagName("html")[0].setAttribute(themeAttrName, newTheme);
 	window.dispatchEvent(new Event(themeChangeEventName));
 }
 
+/**
+ * Returns true if:
+ *   * the local theme is set to 'dark'
+ *   * the local theme is set to 'system' and the system theme is set to 'dark'
+ */
 function isDarkTheme() {
-	return localStorage.getItem(localStorageKey) === darkTheme;
+	const localStorageTheme = getLocalStorageTheme();
+	if (localStorageTheme !== systemTheme) {
+		return localStorageTheme === darkTheme;
+	}
+
+	return getSystemTheme() === darkTheme;
 }
 
-// Call immediately
-(function () {
-	let theme = localStorage.getItem(localStorageKey);
-	if (theme != lightTheme && theme != darkTheme) {
-		// Use dark theme by default
-		theme = darkTheme;
+function getLocalStorageTheme() {
+	let value = localStorage.getItem(localStorageKey);
+	if (!value) {
+		// Use the system theme by default
+		value = systemTheme;
+		localStorage.setItem(localStorageKey, value);
 	}
-	switchTheme(theme);
-})();
+	return value;
+}
+
+function getSystemTheme() {
+	if (window.matchMedia(darkThemeMedia).matches) {
+		return darkTheme;
+	}
+	return lightTheme;
+}
+
+window.matchMedia(darkThemeMedia).addEventListener("change", function () {
+	if (getLocalStorageTheme() !== systemTheme) {
+		// Ignore changes
+		return;
+	}
+
+	updateStyles();
+});
+
+// Update styles on the page load. Don't use 'window.addEventListener("load")' to avoid flickering
+updateStyles();
